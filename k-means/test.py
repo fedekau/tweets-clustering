346d49ded23f7d4c3e8541baa03e3dee43359878
Fabian Larrañaga <fabian.larranaga@wyeworks.com>
2016-09-13 17:44:49 -0300

    Working version

--- k-means/test.py
+++ k-means/test.py
@@ -3,12 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-c1 = np.random.rand(20,2)
-c_1 = np.random.rand(20,2) - 4
-c2 = np.random.rand(20,2) + 10
-c_2 = np.random.rand(20,2) - 10
+top_right = np.random.rand(40,2) + [0, 0]
+top_left = np.random.rand(40,2) + [-1, 1]
+bottom_left = np.random.rand(40,2) + [0, 0]
+bottom_right = np.random.rand(40,2) + [1, -1]
 
-vectors = np.concatenate((c1, c_1, c2, c_2))
+vectors = np.concatenate((top_right, top_left, bottom_left, bottom_right))
 
 res = cl.TFKMeansCluster(vectors, 4)
 
@@ -17,14 +17,39 @@ center2 = res[0][1]
 center3 = res[0][2]
 center4 = res[0][3]
 
-plt.plot(center1[0], center1[1], 'rx')
-plt.plot(center2[0], center2[1], 'gx')
-plt.plot(center3[0], center3[1], 'bx')
-plt.plot(center4[0], center4[1], 'yx')
 
-plt.plot(c1, 'ro')
-plt.plot(c_1, 'go')
-plt.plot(c2, 'bo')
-plt.plot(c_2, 'yo')
+array_1_x = []
+array_1_y = []
+array_2_x = []
+array_2_y = []
+array_3_x = []
+array_3_y = []
+array_4_x = []
+array_4_y = []
+
+for i in range(len(top_right)):
+	array_1_x.append(top_right[i][0])
+	array_1_y.append(top_right[i][1])
+
+	array_2_x.append(top_left[i][0])
+	array_2_y.append(top_left[i][1])
+
+	array_3_x.append(bottom_left[i][0])
+	array_3_y.append(bottom_left[i][1])
+
+	array_4_x.append(bottom_right[i][0])
+	array_4_y.append(bottom_right[i][1])
+
+
+for i in range(len(top_left)):
+	plt.plot(array_1_x, array_1_y, 'ro')
+	plt.plot(array_2_x, array_2_y, 'go')
+	plt.plot(array_3_x, array_3_y, 'co')
+	plt.plot(array_4_x, array_4_y, 'ko')
+
+plt.plot(center1[0], center1[1], 'mx')
+plt.plot(center2[0], center2[1], 'mx')
+plt.plot(center3[0], center3[1], 'mx')
+plt.plot(center4[0], center4[1], 'mx')
 
 plt.show()
