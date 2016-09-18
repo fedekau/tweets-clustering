90999cb52310d5d1f23a41ac9ab1649523e236c8
Federico Kauffman <federico@wyeworks.com>
2016-09-18 09:59:14 -0300

    User local user to connect to database

--- retrieve_tweets/retrieve_tweets_db.py
+++ retrieve_tweets/retrieve_tweets_db.py
@@ -1,8 +1,10 @@
 import psycopg2
 import sys
+import os
 
 def connect_to_db():
-	conn_string = "host='localhost' dbname='tweets-db_development' user='fabian'"
+	user = os.getlogin()
+	conn_string = "host='localhost' dbname='tweets-db_development' user='%(user)s'" % locals()
 
 	# get a connection, if a connect cannot be made an exception will be raised here
 	conn = psycopg2.connect(conn_string)
