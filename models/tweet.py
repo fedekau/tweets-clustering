9e827d1b1659ff6bbdfe14415b3843b5e89d28fa
Federico Kauffman <federico@wyeworks.com>
2016-10-01 14:39:24 -0300

    Remove stopwords

--- models/tweet.py
+++ models/tweet.py
@@ -1,5 +1,6 @@
 from orator import Model
 from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords
 
 class Tweet(Model):
     pass
@@ -7,3 +8,6 @@ class Tweet(Model):
     def tokens(self):
 	tknzr = TweetTokenizer()
 	return tknzr.tokenize(self.data['text'])
+
+    def without_stopwords(self):
+	return [word for word in self.tokens() if word not in stopwords.words('spanish')]
