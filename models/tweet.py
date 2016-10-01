f9d372388aad5393c4885d15669d9e27a6373063
Federico Kauffman <federico@wyeworks.com>
2016-10-01 14:30:32 -0300

    Tokenize tweet text

--- models/tweet.py
+++ models/tweet.py
@@ -1,4 +1,9 @@
 from orator import Model
+from nltk.tokenize import TweetTokenizer
 
 class Tweet(Model):
     pass
+
+    def tokens(self):
+	tknzr = TweetTokenizer()
+	return tknzr.tokenize(self.data['text'])
