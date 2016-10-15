6d40cc749302785ea8768f0c463331c49aea8a74
Federico Kauffman <federico@wyeworks.com>
2016-10-15 16:33:37 -0300

    Random sample tweets

--- src/closest_tweet.py
+++ src/closest_tweet.py
@@ -1,5 +1,5 @@
 import word2vec
-import pdb
+import random
 from models.tweet import Tweet
 
 print "Iniciando"
@@ -12,15 +12,16 @@ def expandir_a_parecidos(tweet):
 
     for t in tokens:
 	t = t.encode('utf8')
+	tweet_expandido.add(t)
 	if model.__contains__(t):
 	    indexes, metrics = model.cosine(t)
-	    parecidos_t = model.vocab[indexes][:3]
+	    parecidos_t = model.vocab[indexes][:1]
 	    tweet_expandido |= set(parecidos_t)
 
     return tweet_expandido
 
-tweets = Tweet.all()[:110]
-target_tweet = tweets[100]
+tweets = random.sample(Tweet.all(), 200)
+target_tweet = tweets[75]
 
 print "Target tweet text"
 print target_tweet.data['text']
@@ -36,9 +37,11 @@ max_palabras_iguales = 0
 for candidate in tweets:
     candidate_expandido = expandir_a_parecidos(candidate)
     palabras_iguales = len(candidate_expandido.intersection(target_tweet_expandido))
-    if palabras_iguales > max_palabras_iguales:
+    if ( palabras_iguales > 0 and palabras_iguales >= max_palabras_iguales) and (candidate.tweet_id != target_tweet.tweet_id):
 	max_palabras_iguales = palabras_iguales
 	closest_tweet = candidate
 	print "Nuevo tweet cercano"
+	print candidate.data['text']
+	print max_palabras_iguales
 	print candidate_expandido
 
