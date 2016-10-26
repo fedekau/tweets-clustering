11b2a926b6f385938db0abac851a5ed073689230
Federico Kauffman <federico@wyeworks.com>
2016-10-25 22:13:38 -0300

    Return splited sentences

--- src/training/iterable_corpus.py
+++ src/training/iterable_corpus.py
@@ -19,16 +19,14 @@ class IterableCorpus(object):
 		fname = os.path.join(self.dirname, fname)
 		if not os.path.isfile(fname):
 		    continue
-		with open(fname) as FileObj:
-		    for lines in FileObj:
-			yield lines
+		for line in open(fname):
+		    yield line.split()
 
     class IterableSingleFileCorpus(object):
 	def __init__(self, fname):
 	    self.fname = fname
 
 	def __iter__(self):
-	    with open(self.fname) as FileObj:
-		for lines in FileObj:
-		    yield lines
+	    for line in open(self.fname):
+	        yield line.split()
 
