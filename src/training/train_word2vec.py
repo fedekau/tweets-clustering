3a6aea77de40d545165ed1db6900931d0b9e82d9
Federico Kauffman <federico@wyeworks.com>
2016-11-05 13:04:10 -0300

    Add workers option

--- src/training/train_word2vec.py
+++ src/training/train_word2vec.py
@@ -10,8 +10,10 @@ def main(argv):
     corpus_path = ''
     model_file = ''
     size = 100
+    workers = 4
+
     try:
-	opts, args = getopt.getopt(argv,"hi:o:s",["ifile=","ofile=","size="])
+	opts, args = getopt.getopt(argv,"hi:o:s:w",["ifile=","ofile=","size=", "workers="])
     except getopt.GetoptError:
 	print 'python train_word2vec.py -i <inputfile> -o <outputfile>'
 	sys.exit(2)
@@ -25,19 +27,22 @@ def main(argv):
 	    model_file = arg
 	elif opt in ("-s", "--size"):
 	    size = int(arg)
+	elif opt in ("-w", "--workers"):
+	    workers = int(arg)
 
     print 'Start training session'
     print 'CORPUS: ' + corpus_path
     print 'MODEL: ' + model_file
     print 'VECTOR SIZE: ' + str(size)
+    print 'WORKERS: ' + str(workers)
 
     sentences = IterableCorpus(corpus_path, preprocessor=LowercasePreprocessor()).sentences()
 
-    model = Word2Vec(sentences, size=size, window=5, min_count=5, workers=4)
+    model = Word2Vec(sentences, size=size, window=5, min_count=5, workers=workers)
 
     model.save(model_file)
 
 if __name__ == "__main__":
-   main(sys.argv[1:10])
+   main(sys.argv[1:])
 
 
