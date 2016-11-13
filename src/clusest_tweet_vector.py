5145807396a29791fce070961c4f5776a386ff33
Federico Kauffman <federico@wyeworks.com>
2016-11-13 17:34:02 -0300

    Add lemmatizer

--- src/clusest_tweet_vector.py
+++ src/clusest_tweet_vector.py
@@ -3,20 +3,22 @@ import random
 from distance.distance import Distance
 from gensim.models import Word2Vec
 from models.tweet import Tweet
+from lemmas.lemmatizer import Lemmatizer
 
 print ("Iniciando")
-model = Word2Vec.load('/Users/federico/proyecto_grado/tweets-clustering/src/training/models/trained_models/W2V-1B-ES-500.bin')
+model = Word2Vec.load('/Users/federico/proyecto_grado/tweets-clustering/src/training/models/PY3-W2V-1B-ES-100.bin')
 print ("Modelo cargado")
 
 tweets = [i for i in Tweet.all() if "@LuisSuarez9" in i.data['text']]
 # tweets = Tweet.all()
-target_tweet = random.sample(tweets, 1)[0]
+target_tweet = Tweet.where('tweet_id', '=', '789158090105126916').first()
 
 print ("---------------------------------------------")
 print ("TARGET TWEET TEXT")
 print (target_tweet.data['text'])
 
-distance = Distance(model)
+l = Lemmatizer('/Users/federico/proyecto_grado/tweets-clustering/src/training/corpora/lemmas/lemmatization-es-no-numbers.txt')
+distance = Distance(model, l)
 
 tweet_ordered = []
 
@@ -30,4 +32,5 @@ def getKey(item):
 tweet_ordered = sorted(tweet_ordered, key=getKey)
 
 for t in tweet_ordered:
-    print("DISTANCE: " + str(t[0]) + " TWEET: " + t[1].data['text'])
+    print("DISTANCE: " + str(t[0]) + "ID: " + str(t[1].tweet_id) + " TWEET: " + t[1].data['text'] + ' ===> TOKENS: ' + str(l.lemmatize(t[1].tokenize_and_clean())))
+
--- src/tweet_representation.py
+++ src/tweet_representation.py
@@ -1,9 +1,14 @@
 from __future__ import print_function
 from models.tweet import Tweet
+from lemmas.lemmatizer import Lemmatizer
 
 tweets = [i for i in Tweet.all() if "@LuisSuarez9" in i.data['text']]
+# tweets = [i for i in Tweet.all() if "@LuisSuarez9" in i.data['text']]
+
+l = Lemmatizer('/Users/federico/proyecto_grado/tweets-clustering/src/training/corpora/lemmas/lemmatization-es-no-numbers.txt')
 
 for candidate in tweets:
     tokens = candidate.tokenize_and_clean()
+    tokens = l.lemmatize(tokens)
     print("TWEET: " + candidate.data['text'] + " ===> " + str(tokens))
     print("---------------------------------------------------------")
