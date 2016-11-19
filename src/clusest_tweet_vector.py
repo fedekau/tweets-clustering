59dda6986523b1e1192b2d9d6d3508dbf5995a5d
Federico Kauffman <federico@wyeworks.com>
2016-11-19 18:19:25 -0300

    Use Person model

--- src/clusest_tweet_vector.py
+++ src/clusest_tweet_vector.py
@@ -5,25 +5,27 @@ from gensim.models import Word2Vec
 from models.tweet import Tweet
 from lemmas.lemmatizer import Lemmatizer
 
+from models.person import Person
 print ("Iniciando")
 model = Word2Vec.load('/Users/federico/proyecto_grado/tweets-clustering/src/training/models/PY3-W2V-1B-ES-100.bin')
 print ("Modelo cargado")
 
-tweets = [i for i in Tweet.all() if "@LuisSuarez9" in i.data['text']]
+tweets = Person.where('name', 'Luis Suarez').first().tweets.all()
 # tweets = Tweet.all()
-target_tweet = Tweet.where('tweet_id', '=', '789158090105126916').first()
+# target_tweet = Tweet.where('tweet_id', '=', '789142606064324608').first()
+target_tweet = random.sample(tweets, 1)[0]
 
 print ("---------------------------------------------")
 print ("TARGET TWEET TEXT")
 print (target_tweet.data['text'])
 
-l = Lemmatizer('/Users/federico/proyecto_grado/tweets-clustering/src/training/corpora/lemmas/lemmatization-es-no-numbers.txt')
-distance = Distance(model, l)
+# l = Lemmatizer('/Users/federico/proyecto_grado/tweets-clustering/src/training/corpora/lemmas/lemmatization-es-no-numbers.txt')
+distance = Distance(model)
 
 tweet_ordered = []
 
 for candidate in tweets:
-    d = distance.vector(candidate, target_tweet)
+    d = distance.cosine(candidate, target_tweet)
     tweet_ordered.append([d, candidate])
 
 def getKey(item):
@@ -32,5 +34,5 @@ def getKey(item):
 tweet_ordered = sorted(tweet_ordered, key=getKey)
 
 for t in tweet_ordered:
-    print("DISTANCE: " + str(t[0]) + "ID: " + str(t[1].tweet_id) + " TWEET: " + t[1].data['text'] + ' ===> TOKENS: ' + str(l.lemmatize(t[1].tokenize_and_clean())))
+    print("DISTANCE: " + str(t[0]) + "ID: " + str(t[1].tweet_id) + " TWEET: " + t[1].data['text'] + ' ===> TOKENS: ' + str(t[1].tokenize_and_clean()))
 
--- src/tweet_representation.py
+++ src/tweet_representation.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
 from models.tweet import Tweet
+from models.person import Person
 from lemmas.lemmatizer import Lemmatizer
 
-tweets = [i for i in Tweet.all() if "@LuisSuarez9" in i.data['text']]
+tweets = Person.where('name', 'Luis Suarez').first().tweets.all()
 # tweets = [i for i in Tweet.all() if "@LuisSuarez9" in i.data['text']]
 
 l = Lemmatizer('/Users/federico/proyecto_grado/tweets-clustering/src/training/corpora/lemmas/lemmatization-es-no-numbers.txt')
